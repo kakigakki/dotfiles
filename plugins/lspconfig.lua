e671afb2857d0f696b4ad174a8cd414b18136b0f
KAKI <kakigakki@yahoo.co.jp>
2023-07-19 02:42:24 +0900

    diffview用起来

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -1,25 +1,26 @@
 vim.diagnostic.config {
   virtual_text = false,
+  signs = true,
 }
 
 local on_attach = require("plugins.configs.lspconfig").on_attach
-local my_on_attach = function(client, bufnr)
-  -- show diagonostic float window when hover
-  vim.api.nvim_create_autocmd("CursorHold", {
-    buffer = bufnr,
-    callback = function()
-      local opts = {
-        focusable = false,
-        close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-        border = "rounded",
-        source = "always",
-        prefix = " ",
-        scope = "cursor",
-      }
-      vim.diagnostic.open_float(nil, opts)
-    end,
-  })
-end
+-- local my_on_attach = function(client, bufnr)
+--   -- show diagonostic float window when hover
+--   vim.api.nvim_create_autocmd("CursorHold", {
+--     buffer = bufnr,
+--     callback = function()
+--       local opts = {
+--         focusable = false,
+--         close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+--         border = "rounded",
+--         source = "always",
+--         prefix = " ",
+--         scope = "cursor",
+--       }
+--       vim.diagnostic.open_float(nil, opts)
+--     end,
+--   })
+-- end
 
 local capabilities = require("plugins.configs.lspconfig").capabilities
 capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
@@ -33,11 +34,12 @@ capabilities.textDocument.foldingRange = {
 local lspconfig = require "lspconfig"
 
 -- for vue2
--- local servers = { "html", "cssls", "vuels", "tsserver", "clangd" }
+-- local servers = { "html", "cssls", "vuels", "tsserver"}
 
 -- for vue3
-local servers = { "cssls", "clangd", "volar" }
+local servers = { "cssls", "volar", "eslint_d" }
 
+-- when use take over mode
 lspconfig.volar.setup {
   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
 }
@@ -46,7 +48,7 @@ for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = function(client, bufnr)
       on_attach(client, bufnr)
-      my_on_attach(client, bufnr)
+      -- my_on_attach(client, bufnr)
     end,
     capabilities = capabilities,
   }
