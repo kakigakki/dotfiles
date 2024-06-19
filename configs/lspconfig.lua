c72ee292af20ebf010cc1a9ecb8401d94533bc21
KAKI <kakigakki@yahoo.co.jp>
2024-06-19 17:58:37 +0900

    add ruby env

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -34,7 +34,7 @@ local lspconfig = require "lspconfig"
 -- local servers = { "html", "cssls", "vuels", "tsserver" }
 
 -- for vue3
-local servers = { "cssls", "volar", "tsserver" }
+local servers = { "cssls", "volar", "tsserver", "solargraph" }
 -- local capabilities = require("plugins.configs.lspconfig").capabilities -- when use take over mode
 -- capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true -- when use take over mode
 -- lspconfig.volar.setup {
@@ -47,16 +47,39 @@ for _, lsp in ipairs(servers) do
       on_attach(client, bufnr)
       -- my_on_attach(client, bufnr)
     end,
-    capabilities = capabilities,
+    -- capabilities = capabilities,
   }
 end
 
+-- eslint
 lspconfig.eslint.setup {
   --- ...
   on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       command = "EslintFixAll",
+    })
+  end,
+}
+
+--rubocop
+lspconfig.rubocop.setup {
+  on_attach = function(client, bufnr)
+    vim.opt.signcolumn = "yes"
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "ruby",
+      callback = function()
+        vim.lsp.start {
+          name = "rubocop",
+          cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        }
+      end,
+    })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.rb",
+      callback = function()
+        vim.lsp.buf.format()
+      end,
     })
   end,
 }
